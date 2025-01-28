1e6591bf3f879777b160947619e1ff65b74350a3
Ihar Zakharau <ihar_zakharau@epam.com>
2025-01-28 15:25:29 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// For help writing plugins, visit the documentation to get started:
+//   https://docs.insomnia.rest/insomnia/introduction-to-plugins
+// Deploy path is C:\Users\Ihar_Zakharau\AppData\Roaming\Insomnia\plugins\insomnia-plugin-crypto
+
+// TODO: Add plugin code here...
