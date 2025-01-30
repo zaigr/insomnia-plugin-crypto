e6ceb76a33de7db764d6b2c8ac4fef800aa1c595
Ihar Zakharau <ihar_zakharau@epam.com>
2025-01-30 21:47:19 +0100

    add request body encryption

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const {
   enableResponseDecryption,
   isResponseDecryptionEnabled,
+  enableRequestEncryption,
+  isRequestEncryptionEnabled
 } = require('./store');
 
-const { decrypt } = require('./encrypt');
+const {
+  encrypt,
+  decrypt,
+} = require('./encrypt');
 
 module.exports.responseHooks = [
   async (context) => {
@@ -28,9 +33,32 @@ module.exports.responseHooks = [
   }
 ];
 
+module.exports.requestHooks = [
+  async (context) => {
+    const enabled = await isRequestEncryptionEnabled(context.store, context.request.getId());
+    if (enabled) {
+      const algorithm = context.request.getEnvironmentVariable('crypto-alg');
+      const key = context.request.getEnvironmentVariable('crypto-key');
+
+      if (!algorithm || !key) {
+        console.error('Encryption failed: Missing algorithm or key in environment');
+        return;
+      }
+
+      try {
+        const encryptedBody = encrypt(context.request.getBody().text, algorithm, key);
+        context.request.setBodyText(encryptedBody);
+      } catch (error) {
+        console.error('Encryption failed:', error.message);
+        throw error;
+      }
+    }
+  }
+];
+
 module.exports.requestActions = [
   {
-    label: 'Toggle Decryption',
+    label: 'Toggle Response Decryption',
     action: async (context, data) => {
       const { store } = context;
       const { request } = data;
@@ -39,8 +67,22 @@ module.exports.requestActions = [
 
       const newState = !currentState;
       await enableResponseDecryption(store, request._id, newState);
-      
-      context.app.alert('Crypto', `Decryption ${newState ? 'enabled' : 'disabled'} for this request`);
+
+      context.app.alert('Crypto', `Response decryption ${newState ? 'enabled' : 'disabled'} for this request`);
+    }
+  },
+  {
+    label: 'Toggle Request Encryption',
+    action: async (context, data) => {
+      const { store } = context;
+      const { request } = data;
+
+      const currentState = await isRequestEncryptionEnabled(store, request._id);
+
+      const newState = !currentState;
+      await enableRequestEncryption(store, request._id, newState);
+
+      context.app.alert('Crypto', `Request encryption ${newState ? 'enabled' : 'disabled'} for this request`);
     }
   }
 ];
--- store.js
+++ store.js
@@ -1,4 +1,5 @@
 const enableDecryptionKey = 'enableDecryption';
+const enableEncryptionKey = 'enableEncryption';
 
 const enableResponseDecryption = async (store, itemId, enable) => {
   const key = `${enableDecryptionKey}:${itemId}`;
@@ -12,7 +13,21 @@ const isResponseDecryptionEnabled = async (store, itemId) => {
   return storeValue === 'true' ? true : false;
 };
 
+const enableRequestEncryption = async (store, itemId, enable) => {
+  const key = `${enableEncryptionKey}:${itemId}`;
+  await store.setItem(key, enable);
+};
+
+const isRequestEncryptionEnabled = async (store, itemId) => {
+  const key = `${enableEncryptionKey}:${itemId}`;
+  const storeValue = await store.getItem(key);
+
+  return storeValue === 'true' ? true : false;
+};
+
 module.exports = {
   enableResponseDecryption,
   isResponseDecryptionEnabled,
+  enableRequestEncryption,
+  isRequestEncryptionEnabled,
 };
